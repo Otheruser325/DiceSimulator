f8194d2d1801486e327d6b1adcd7be3af77fc0c1
Otheruser325 <btdbmodder@gmail.com>
2025-11-29 00:11:50 +0000

    Update 1.2
    Update 1.2

--- js/main.js
+++ js/main.js
@@ -73,13 +73,13 @@ function create() {
 	
 	// Custom Dice UI
     this.sidesInput = createInputField(
-        this, 100, 200,
+        this, 900, 260,
         "Enter sides...",
         { fontSize: "28px", fontFamily: "Verdana", color: "#fff" }
     ).setVisible(false);
 
     this.luckInput = createInputField(
-        this, 100, 260,
+        this, 900, 320,
         "Enter luck factor...",
         { fontSize: "28px", fontFamily: "Verdana", color: "#fff" }
     ).setVisible(false);
